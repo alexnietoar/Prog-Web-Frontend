c0a1863cce4c3954fcf1d585653c519a6480e0e1
Santi <santifl.17@gmail.com>
2020-05-06 06:27:01 -0500

    Agregando nuevo modelo, servicios y modificando componentes
    + Modelos Producto
    + Servicio Producto y Mensaje
    + Editando Componentes Cart y Product-list

---
+++ src/app/models/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+
+    constructor(id, name, description = '', price = 0, imageUrl = 'https://www.preciosonline.co/wp-content/uploads/2017/09/pTRUCA1-25913242enh-z6.jpg') {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.price = price
+        this.imageUrl = imageUrl
+    }
+}
